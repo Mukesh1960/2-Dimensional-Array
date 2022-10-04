14dbfd4d945689da1a266b259dd4163cd3eaf4df
Mukesh1960 <114616008+Mukesh1960@users.noreply.github.com>
2022-10-04 19:40:14 +0530

    Add files via upload
    a java code showing 2-D Array

---
+++ Array2dIO.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Array2dIO {
+
+    public static Scanner scn = new Scanner (System.in);
+
+    public static int[][] input2(int n, int m) {
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        return arr;
+    }
+
+    public static void input1 (int[][] arr) {
+        int n = arr.length;
+        int m = arr[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        } 
+    }
+
+    public static void display(int[][] arr) {
+        int n = arr.length;
+        int m = arr[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main (String[] args) {
+        int[][] arr = new int[scn.nextInt()][scn.nextInt()];
+        input1(arr);
+        display(arr);
+    }
+} 
